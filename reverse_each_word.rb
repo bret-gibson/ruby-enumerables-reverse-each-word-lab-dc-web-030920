42cc4e9ce3ffcf24e0f83b65c1ed35e4d330ddfe
Bret Gibson <bret.gibson@gmail.com>
2020-03-08 02:53:32 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,12 +3,14 @@ require 'pry'
 def reverse_each_word(sentence)
 sentenceArray = sentence.split(" ")
 returnString = ""
+count
 sentenceArray.each do |ele|
-  if sentenceArray
+  if sentenceArray.length == count
     returnString += "#{ele.reverse}"
   else
     returnString += "#{ele.reverse} "
   end
+  count +=1
   puts ele.reverse
   ele.reverse
 end
