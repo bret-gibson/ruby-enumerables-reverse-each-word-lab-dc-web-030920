b386106e8d3aa62cddbbf8686da63320258dd249
Bret Gibson <bret.gibson@gmail.com>
2020-03-08 02:31:33 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,6 +4,13 @@ def reverse_each_word(sentence)
 sentenceArray = sentence.split(" ")
 sentenceArray.reverse.each do |ele|
   tempArr = ele.split("")
+  count = 0
+  index = -1
+  while count < tempArr.length do
+    puts tempArr[index]
+    index -= 1
+    count +=1
+  end
   puts tempArr
 end
 =begin
